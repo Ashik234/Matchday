673fe5bece8b363e5b9e4a5bffb328bb14fb2a13
ashik-dignizant <121389068+Ashik234@users.noreply.github.com>
2026-06-04 15:48:44 +0530

    routes: add /team/:slug behind shared Layout

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,11 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from '@/App';
+import TeamPage from '@/pages/TeamPage';
+import { Layout } from '@/pages/Layout';
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route
+          path="/team/:slug"
+          element={
+            <Layout>
+              <TeamPage />
+            </Layout>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
